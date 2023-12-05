a2b08d0ac4d89bdd906d1553543113722ad300be
udaykompella <kompellauday9@gmail.com>
2023-12-06 00:34:10 +0530

    problem in querying

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,31 +2,53 @@ const fs = require('fs');
 const Tour = require('./../models/TourModel');
 
 // const tours = JSON.parse(fs.readFileSync(`./dev-data/data/tours-simple.json`));
-exports.updateTour = async(req, res) => {
-  try{
-    const tours = await Tour.findByIdAndUpdate(req.params.id,req.body,{
-      new:true,
-      runValidators:true 
-    })
-  
+exports.updateTour = async (req, res) => {
+  try {
+    const tours = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
     res.status(200).json({
       status: 'success',
-      data:{
-        tour:tours
-      }
+      data: {
+        tour: tours,
+      },
     });
-  }catch(error){
+  } catch (error) {
     res.status(400).json({
-      status:"fail",
-      message:error
-    })
+      status: 'fail',
+      message: error,
+    });
   }
-  
 };
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
-    console.log(tours,"gettours")
+    //Build Query
+    // 1) Filtering
+    console.log(req.query,"sdsss")
+    let queryObj = { ...req.query };
+
+    let excludedFields = ['page', 'limit', 'sort', fields];
+
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    //2) Advance Filtering
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      match => `$${match}`
+    );
+
+    console.log(JSON.parse(queryString));
+
+    const query = await Tour.find(queryObj);
+
+    const tours = await query
+
+    // const tours = await Tour.find().where('duration').equals(5).where('difficult').equals('easy')
+
+    console.log(tours, 'gettours');
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -41,20 +63,20 @@ exports.getAllTours = async (req, res) => {
     });
   }
 };
-exports.getTour = async(req, res) => {
-  try{
-    const tour = await Tour.findById(req.params.id)
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
     res.status(200).json({
-      status:'success',
-      data:{
-        tour
-      }
-    })
-  }catch(error){
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
     res.status(400).json({
-       status:'fail',
-       message:error
-    })
+      status: 'fail',
+      message: error,
+    });
   }
 
   // console.log(tour)
@@ -72,18 +94,18 @@ exports.createTour = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent',
+      message: error,
     });
   }
 
   const newTour = await Tour.create(req.body);
   // res.send('Done')
 };
-exports.deleteTour = async(req, res) => {
-  try{
-    await Tour.findByIdAndDelete(req.params.id)
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
     res.status(201).json({});
-  }catch(error){
+  } catch (error) {
     res.status(400).json({
       status: 'fail',
       message: error,
--- server.js
+++ server.js
@@ -3,24 +3,22 @@ const dotenv = require('dotenv');
 const app = require('./app');
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+const DB = "mongodb+srv://kompellauday9:uday@cluster0.kp5pxnf.mongodb.net/NATOURS"
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true 
   })
   .then((con) => {
-    console.log(con.connections);
+    // console.log(con.connections);
     console.log('db connection successful');
   });
 
 
-console.log(process.env);
+// console.log(process.env);
 const port = 3000;
 
 app.listen(port, () => {

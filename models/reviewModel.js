44535c46e4d6fbaa69eedbbc1915cf462c63515e
udaykompella <kompellauday9@gmail.com>
2024-02-02 22:34:44 +0530

    populating reviews and virtual populate

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -25,12 +25,31 @@ const reviewSchema = new mongoose.Schema(
       ref: "User",
       required: [true, "Review must belong to a user"],
     },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
-  //   {
-  //     toJSON: { virtuals: true },
-  //     toObject: { virtuals: true },
-  //   }
 );
+
+// reviewSchema.pre(/^find/, function (next) {
+//   this.populate({
+//     path: "tour",
+//     select: "name",
+//   }).populate({
+//     path: "user",
+//     select: "name photo",
+//   });
+//   next();
+// });
+
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "user",
+    select: "name photo",
+  });
+  next();
+});
 
 const Review = mongoose.model("Review", reviewSchema);
 

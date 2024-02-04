c28aee9cd2c04a7ddfae5f5a126d1b95b314e649
udaykompella <kompellauday9@gmail.com>
2024-02-04 23:27:11 +0530

    setting up pug templates

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
+const path = require("path");
 const express = require("express");
 const fs = require("fs");
 const app = express();
 const AppError = require("./utils/appError");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
-const mongoSanitize = require('express-mongo-sanitize')
-const xss = require('xss-clean')
-const hpp = require('hpp')
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 const tourRouter = require("./Routes/tourRoutes");
 const userRouter = require("./Routes/userRoutes");
 const globalErrorHandler = require("./controllers/errorController");
 const reviewRoute = require("./Routes/reviewRouter");
 // const globalErrorHandler = require("./dev-data/data/import-dev-data");
 
+app.set("view engine", "pug");
+
+app.set("views", path.join(__dirname, "views"));
+
+//serving static filess
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, "public")));
 //1)GLOBAL MIDDLEWARES
 //set security http headers
 app.use(helmet());
@@ -51,15 +59,15 @@ app.use(
   })
 );
 
-//serving static filess
-app.use(express.static(`${__dirname}/public`));
-
 //Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+app.get("/", (req, res) => {
+  res.status(200).render("base");
+});
 app.use("/api/v1/tours", tourRouter); // This acts as a middleware that is when new request hits it goes into middleware stack
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRoute);

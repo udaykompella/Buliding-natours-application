306775c86f1017c1779c27c35e9af23aaa75d54a
udaykompella <kompellauday9@gmail.com>
2024-02-21 22:44:55 +0530

    account settings part2 completed

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,13 @@
 import "@babel/polyfill";
 import { login, logout } from "./login";
 import { displayMap } from "./mapbox";
+import { updateData } from "./updateSettings";
 
 //Dom Elements
 const mapBox = document.getElementById("map");
 const loginForm = document.querySelector(".form--login");
 const logOutBtn = document.querySelector(".nav__el--logout");
+const userDataForm = document.querySelector(".form-user-data");
 
 //values
 
@@ -27,3 +29,12 @@ if (loginForm) {
 }
 
 if (logOutBtn) logOutBtn.addEventListener("click", logout);
+
+if (userDataForm) {
+  userDataForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const name = document.getElementById("name").value;
+    updateData(name, email);
+  });
+}
---
+++ public/js/updateSettings.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { showAlert } from "./alerts";
+export const updateData = async (name, email) => {
+  try {
+    const res = await axios({
+      method: "PATCH",
+      url: "/api/v1/users/updateMe",
+      data: {
+        name,
+        email,
+      },
+    });
+    if (res.data.status == "success") {
+      showAlert("success", "Data update successfully!!");
+    }
+  } catch (error) {
+    showAlert("error", error.response.data.message);
+  }
+};

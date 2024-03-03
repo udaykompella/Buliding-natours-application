0fc42834ec90d4833cf4d0542932d36b0273c000
udaykompella <kompellauday9@gmail.com>
2024-03-03 21:56:12 +0530

    Bookings model

---
+++ models/bookingModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Tour",
+    required: [true, "Booking must belong to a Tour"],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: [true, "Booking must belong to a User"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  paid: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+bookingSchema.pre(/^find/, function (next) {
+  this.populate("user").populate({
+    path: "tour",
+    select: "name",
+  });
+});
+
+const booking = mongoose.model("Booking", bookingSchema);
+
+module.exports = booking;
